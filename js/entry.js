910efefb3bc23083af7f3a45407642b913ac9252
wb-sfzy189670 <wb-sfzy189670@alibaba-inc.com>
2017-02-27 15:42:42 +0800

    comment

---
+++ js/entry.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import ParentNode from './index.js';
+
+
+ReactDom.render(
+    <ParentNode></ParentNode>,
+    document.getElementById('react')
+);
